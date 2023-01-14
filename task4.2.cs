130e91e8dd8ce360caeff63c5559d9dfb34eb0c6
LifeToHell <120266818+LifeToHell@users.noreply.github.com>
2023-01-14 17:02:02 +0400

    Add files via upload

---
+++ task4.2.cs
@@ -0,0 +1,24 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+
+
+Console.Clear();
+
+Console.WriteLine("Задача 27");
+Console.Write("Напишите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (n > 0)
+{
+   sum += n % 10;
+   n = n / 10;
+   }
+
+Console.Write(sum);
+
